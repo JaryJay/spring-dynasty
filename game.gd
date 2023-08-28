1e514d254068f31c08ec012bf637816dc7d8dcc3
Dissonant101 <92066010+Dissonant101@users.noreply.github.com>
2023-08-28 11:29:49 -0400

    Refactor pause menu to no longer access parent

--- game.gd
+++ game.gd
@@ -2,20 +2,23 @@ extends Node2D
 
 class_name Game
 
-signal client_pause_state_changed(client_is_paused: bool)
-var client_pause_state: bool = false:
-	get:
-		return client_pause_state
-	set(value):
-		client_pause_state = value
-		emit_signal("client_pause_state_changed", client_pause_state)
-
 @onready var selection_rect: SelectionRect = $SelectionRect
+@onready var pause_menu: Control = $CanvasLayer/PauseMenu
 
+var client_is_paused: bool = false
 var selected_squads: Array[Squad] = []
 var controlled_team: int = 0
 
+func _ready():
+	var pause_menu_resume_button: Button = $CanvasLayer/PauseMenu/Panel/VBoxContainer/Resume
+	pause_menu_resume_button.pressed.connect(_on_pause_menu_resume_pressed)
+
 func _process(_delta):
+	if client_is_paused:
+		pause_menu.show()
+	else:
+		pause_menu.hide()
+	
 	if selection_rect.is_selecting:
 		for selected_squad in selected_squads:
 			selected_squad.selected = false
@@ -30,10 +33,13 @@ func _process(_delta):
 
 func _input(_event):
 	if _event.is_action_pressed("ui_cancel"):
-		client_pause_state = !client_pause_state
+		client_is_paused = !client_is_paused
 	
 	var selecting: = Input.is_action_pressed("select")
 	if Input.is_action_pressed("primary") and not selecting:
 		var mouse_pos: = get_global_mouse_position()
 		for squad in selected_squads:
 			squad.set_target_position(mouse_pos)
+
+func _on_pause_menu_resume_pressed():
+	client_is_paused = false
--- ui/pause_menu.gd
+++ ui/pause_menu.gd
@@ -1,26 +1,13 @@
 extends Control
 
-@export var game: Game
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	hide()
-	game.connect("client_pause_state_changed", _on_game_client_pause_state_changed)
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
-func _on_game_client_pause_state_changed(client_is_paused: bool):
-	if client_is_paused:
-		show()
-	else:
-		hide()
-
-
-func _on_resume_pressed():
-	game.client_pause_state = false
 
 
 func _on_exit_pressed():
