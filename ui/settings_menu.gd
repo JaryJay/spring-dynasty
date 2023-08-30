f73480448bea3f2110bf2d3264900e3825b76e4e
zhanc2 <76535472+zhanc2@users.noreply.github.com>
2023-08-29 20:37:49 -0400

    Settings Menu outline and back/exit buttons

---
+++ ui/settings_menu.gd
@@ -0,0 +1,27 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_account_settings_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_account_settings_button_toggled(button_pressed):
+	pass # Replace with function body.
+
+
+func _on_back_button_pressed():
+	get_tree().change_scene_to_file("res://ui/main_menu.tscn")
+
+
+func _on_quit_button_pressed():
+	get_tree().quit()
