5043fffccd74955f34ca94c9a5ca296cdc51741d
Jay <jay.jrjren@gmail.com>
2023-12-01 22:59:55 -0500

    Give basic defensive ai to controllable squads
    When there is an enemy within attacking distance

--- entities/squads/states/idle_state.gd
+++ entities/squads/states/idle_state.gd
@@ -1,6 +1,8 @@
 extends State
 class_name IdleState
 
+@export var attacking_state: AttackingState
+
 func _enter_state(squad: Squad) -> void:
 	# Uncomment the following line to debug
 #	squad.debug_label.show()
@@ -12,6 +14,7 @@ func _enter_state(squad: Squad) -> void:
 
 func process(squad: Squad) -> void:
 	handle_pushing(squad)
+	detect_enemies(squad)
 
 func handle_pushing(squad: Squad) -> void:
 	var push_area: Area2D = squad.push_area
@@ -34,6 +37,21 @@ func handle_pushing(squad: Squad) -> void:
 		avg_push_dir = (avg_push_dir / pushing_count)
 		if not avg_push_dir.is_zero_approx():
 			squad.rotate_and_move(avg_push_dir.normalized(), 0.5)
+
+func detect_enemies(squad: Squad) -> void:
+	var closest_enemy: Squad
+	var closest_dist: float = INF
+	
+	for body in squad.awareness_area.get_overlapping_bodies():
+		if not body is Squad or body.team == squad.team or not body.is_alive():
+			continue
+		if squad.position.distance_squared_to(body.position) < closest_dist:
+			closest_dist = squad.position.distance_squared_to(body.position)
+			closest_enemy = body
+	
+	if closest_enemy and closest_dist < squad.engage_range ** 2:
+		attacking_state.target = closest_enemy
+		squad.state_machine.state = attacking_state
 
 func _exit_state(squad: Squad) -> void:
 	squad.debug_label.hide()
